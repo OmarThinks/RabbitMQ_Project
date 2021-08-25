9c04623cb7dc290f910444c141cb711e17690308
Omar Magdy <contact.parmag@gmail.com>
2021-08-25 17:52:45 +0200

    receiver.py:acknoledged the receiving of data

--- receiver.py
+++ receiver.py
@@ -8,17 +8,28 @@ channel = connection.channel()
 
 
 
+def ack_message(channel, method):
+    """Note that `channel` must be the same pika channel instance via which
+    the message being ACKed was retrieved (AMQP protocol constraint).
+    """
+    if channel.is_open:
+        channel.basic_ack(method.delivery_tag)
+    else:
+        # Channel is already closed, so we can't ACK this message;
+        # log and/or do something that makes sense for your app in this case.
+        pass
+
+
+
+
 def callback(channel,method, properties, body):
+	ack_message(channel,method)
 	print("body",body, flush=True)
 
 
-
-
 channel.basic_consume(
-	queue="hello", on_message_callback=callback, auto_ack=True)
-
+	queue="hello", on_message_callback=callback)
 
 channel.start_consuming()
-
 
 connection.close()
