68f08b7f785964b19a8b8dd055bd43192c6fc063
Omar Magdy <contact.parmag@gmail.com>
2021-08-25 18:36:53 +0200

    services deleted

--- Services/service_products/products/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.CharField(max_length=3000)
-    price = models.FloatField()
-
-
